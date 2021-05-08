5b7e779d382da5a0c47cfe94e2bcd1a673af21bc
Valentin <valentingraglia18@gmail.com>
2021-05-08 10:40:51 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="body_container">
       <Navbar/>
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Header/header.css'
+
+const Header = () => {
+    return (
+        <div className='title_container'>
+            <h1 className='title'>Valentin Graglia</h1>
+            <h2 className='sub_title'>Desarrolador de front-end</h2>
+        </div>
+    )
+}
+
+export default Header
